3d6484f862ec560f70f42efbcf987949a21dfa1a
reischlfranz <reischl.franz@gmail.com>
2018-12-20 10:27:43 +0100

    Included Folder archiving

--- __#__archive__#__.bat
+++ __#__archive__#__.bat
@@ -1,5 +1,5 @@
 @REM miniVCS-winBatch
-@REM Drag-and-Drop archiving a list of files
+@REM Drag-and-Drop archiving a list of files and folders
 @REM https://github.com/reischlfranz/miniVCS-winBatch
 @echo off
 
@@ -9,6 +9,9 @@ SETLOCAL
 @REM --------------------------------
 @REM Change these to your situation
 SET ARCHIVEPATH=_archiv\
+SET ZIPPATH="C:\Program Files\7-Zip\7z.exe"
+SET ZIPPARMS= a -r 
+SET ZIPEXT=.zip
 
 
 SET WORKPATH=%~dp0
@@ -40,10 +43,27 @@ for %%i in (%*) do (
 		@REM echo full path : %%~pi
 		@REM echo file path : %%~dpnxi
 		
-		@REM '*' at the end of new file name tricks xcopy into treating the path as file, rather than ask for confirmation whether it is a file or directory
-		xcopy /Q /H "%%~dpnxi" "%WORKPATH%%ARCHIVEPATH%%%~ni_%DT%-%TM%%%~xi*"
+
+		@REM File is directory?
+		IF EXIST %%~si\* (
+			@REM This is a directory: %%i
+			%ZIPPATH% %ZIPPARMS% "%WORKPATH%%ARCHIVEPATH%%%~ni_%DT%-%TM%%ZIPEXT%" "%%~dpnxi"
+		)
+		IF NOT EXIST %%~si\* (
+			@REM This is a file: %%i
+			@REM '*' at the end of new file name tricks xcopy into treating the path as file, rather than ask for confirmation whether it is a file or directory
+			xcopy /Q /H "%%~dpnxi" "%WORKPATH%%ARCHIVEPATH%%%~ni_%DT%-%TM%%%~xi*"
+		)
 	)
+	
 )
+
+
+
+
+
+
+
 
 :end
 
