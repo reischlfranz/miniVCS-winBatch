569e8819526cdfa703a6769e0107558a7d3135e6
reischlfranz <reischl.franz@gmail.com>
2019-01-10 10:50:04 +0100

    Option to use Powershell ZIP; Select how to handle files.
    For situations where no external zip utility is available. Can be switched between.
    Also, there is now an option to either just copy files or zip them as well.

--- __#__archive__#__.bat
+++ __#__archive__#__.bat
@@ -7,11 +7,21 @@ SETLOCAL
 @REM --------------------------------
 @REM ---   Individual variables   ---
 @REM --------------------------------
-@REM Change these to your situation
+@REM --- Change these to your situation
 SET ARCHIVEPATH=_archiv\
+SET ZIPEXT=.zip
+
+@REM --- Set to ZIP to Zip single files, set to COPY to just copy them
+SET FILEACTION=COPY
+
+@REM --- Set to PS to use PowerShell Zip functionality, set to ZIP to use external zip tool (see below)
+SET USEZIP=PS
+
+@REM --- Path to and parameters of external ZIP utility
 SET ZIPPATH="C:\Program Files\7-Zip\7z.exe"
 SET ZIPPARMS= a -r 
-SET ZIPEXT=.zip
+
+
 
 
 SET WORKPATH=%~dp0
@@ -47,23 +57,38 @@ for %%i in (%*) do (
 		@REM File is directory?
 		IF EXIST %%~si\* (
 			@REM This is a directory: %%i
-			%ZIPPATH% %ZIPPARMS% "%WORKPATH%%ARCHIVEPATH%%%~ni_%DT%-%TM%%ZIPEXT%" "%%~dpnxi"
+			echo This is a directory: %%i
+			IF %USEZIP% EQU PS (
+				powershell -command "Compress-Archive -Path '%%~dpnxi' -CompressionLevel Optimal -DestinationPath '%WORKPATH%%ARCHIVEPATH%%%~ni_%DT%-%TM%%ZIPEXT%'"
+			)
+			IF %USEZIP% EQU ZIP (
+				%ZIPPATH% %ZIPPARMS% "%WORKPATH%%ARCHIVEPATH%%%~ni_%DT%-%TM%%ZIPEXT%" "%%~dpnxi"
+			)
+
 		)
 		IF NOT EXIST %%~si\* (
 			@REM This is a file: %%i
-			@REM '*' at the end of new file name tricks xcopy into treating the path as file, rather than ask for confirmation whether it is a file or directory
-			xcopy /Q /H "%%~dpnxi" "%WORKPATH%%ARCHIVEPATH%%%~ni_%DT%-%TM%%%~xi*"
+			echo This is a file: %%i
+			IF %FILEACTION% EQU ZIP (
+				echo Trying to zip...
+				IF %USEZIP% == PS (
+					powershell -command "Compress-Archive -Path '%%~dpnxi' -CompressionLevel Optimal -DestinationPath '%WORKPATH%%ARCHIVEPATH%%%~ni_%%~xi.%DT%-%TM%%ZIPEXT%'"
+				)
+				IF %USEZIP% EQU ZIP (
+					%ZIPPATH% %ZIPPARMS% "%WORKPATH%%ARCHIVEPATH%%%~ni_%%~xi.%DT%-%TM%%ZIPEXT%" "%%~dpnxi"
+				)
+			)
+			IF %FILEACTION% EQU COPY (
+				@REM '*' at the end of new file name tricks xcopy into treating the path as file, rather than ask for confirmation whether it is a file or directory
+				xcopy /Q /H "%%~dpnxi" "%WORKPATH%%ARCHIVEPATH%%%~ni_%DT%-%TM%%%~xi*"
+
+				@REM (Optional) "Touch" the archived file to update the time stamp
+				copy /B "%WORKPATH%%ARCHIVEPATH%%%~ni_%DT%-%TM%%%~xi"+,, "%WORKPATH%%ARCHIVEPATH%%%~ni_%DT%-%TM%%%~xi" /Y
+			)
 		)
 	)
 	
 )
-
-
-
-
-
-
-
 
 :end
 
