1cfbc329eb7a322d0a9e57a0236eee545414ffa2
reischlfranz <reischl.franz@gmail.com>
2019-02-13 11:01:57 +0100

    Added help message
    Will be displayed when no parameter is given or with parameter '/?'
    
    Addresses issue #1

--- __#__archive__#__.bat
+++ __#__archive__#__.bat
@@ -22,6 +22,14 @@ SET USEZIP=PS
 SET ZIPPATH="C:\Program Files\7-Zip\7z.exe"
 SET ZIPPARMS= a -r 
 
+:: No parameters given - print help
+IF NOT EXIST %%0 GOTO help
+
+:: Check for parameter '/?'
+for %%i in (%*) do (
+  IF (%%i EQU "/?") goto help
+)
+
 SET "WORKPATH=%~dp0"
 IF NOT EXIST "%WORKPATH%%ARCHIVEPATH%" mkdir "%WORKPATH%%ARCHIVEPATH%"
 
@@ -83,7 +91,44 @@ for %%i in (%*) do (
   )
   
 )
+GOTO end
+
+:help
+
+echo Saves and archives copies of files complete with current timestamp into archive directory.
+echo.
+echo %~nx0 [file1 [file2 file3 ...]]
+echo.
+echo.  /?         Display this help message
+echo.
+echo How to use:
+echo Drag-and-Drop files and directory in Windows Explorer onto this batch file.
+echo Alternatively, use in terminal with above given syntax.
+echo. 
+echo Settings:
+echo Change these lines with a text editor to suit your situation:
+echo.   ...
+echo.   :: Change these to your situation
+echo.   SET ARCHIVEPATH=_archiv\
+echo.   SET ZIPEXT=.zip
+echo.   
+echo.   :: Set to ZIP to Zip single files, set to COPY to just copy them
+echo.   SET FILEACTION=COPY
+echo.   
+echo.   :: Set to PS to use PowerShell Zip functionality
+echo.   :: Set to ZIP to use external zip tool (see below)
+echo.   SET USEZIP=PS
+echo.   
+echo.   :: Path to and parameters of external ZIP utility
+echo.   SET ZIPPATH="C:\Program Files\7-Zip\7z.exe"
+echo.   SET ZIPPARMS= a -r 
+echo.   ...
+
+
+pause
+GOTO end
 
 :end
 
 ENDLOCAL
+
